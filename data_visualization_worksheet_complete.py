f9b037ea744c23f8b3dc4a62e35e27da77679c60
jirivalek01 <jiri.valek01@gmail.com>
2025-12-14 16:41:14 +0100

    Update data_visualization_worksheet_complete.py

--- data_visualization_worksheet_complete.py
+++ data_visualization_worksheet_complete.py
@@ -13,20 +13,12 @@ df_products = pd.read_csv(DATA_PATH)
 # Výběr relevantních sloupců a kopie
 df_products_subset = df_products[['Ukazatel', 'Reprezentant', 'CasM', 'Hodnota']].copy()
 
-# Převod sloupce CasM na datetime a seřazení
+# Převod sloupce CasM na datetime
 df_products_subset['CasM'] = pd.to_datetime(df_products_subset['CasM'], errors='coerce')
-df_products_subset = df_products_subset.sort_values('CasM')
-
-# Filtr pro Pšenici
-df_psenice = df_products_subset[df_products_subset['Reprezentant'].str.contains("Pšenice", na=False)]
 
 # Streamlit titul
 st.title("Vizualizace dat zemědělských produktů")
 
-# Graf pro Pšenici
-st.subheader("Hodnoty pro Pšenici")
-fig_psenice = px.line(df_psenice, x='CasM', y='Hodnota', color='Ukazatel')
-st.plotly_chart(fig_psenice, use_container_width=True)
 
 # Filtr pro průměrné ceny
 df_products_subset_mean = df_products_subset[df_products_subset['Ukazatel'].str.contains("Průměrná cena", na=False)]
